3c76654df2ef2825897af4b78251a6416048a8b6
Hitenop <87300913+Hitenop@users.noreply.github.com>
2021-08-16 13:31:55 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -3,10 +3,10 @@
 class Presets(object):
     CAPTION_TEXT_DOC = "\n\n<b>File Name:</b> {}\n\n<b>Format:</b> {}\n<b>Size:</b> {}"
     CAPTION_TEXT_VID = "\n\n<b>File Name:</b> {}\n\n<b>Size:</b> {}"
-    ASK_PM_TEXT = "<b>Click the below button</b>"
-    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>I can help you getting movies from</code> @MovieKeralam. " \
-                   "<code>Just Keep this message live Here</code>😉\n\n" \
-                   "<b>My code can be seen: </b><a href='https://github.com/m4mallu/gofilesbot'> HERE</a>"
+    ASK_PM_TEXT = "<b>Click the below button
+@request_moviesorseries </b>"
+    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>I can help you getting movies from</code> @request_moviesorseries. " \
+                   
     CLEAN_CHAT_MSG = "⚠️ <b>Deleting all messages..</b>"
     MSG_FOR_PIN = "<b>For getting medias from here..</b>\n\n🔛 <code>Please start</code> @{} <code>in PM\n\n" \
                   "Send the exact Movie name.\n\n🔊 I'll reply the file in PM if available in our channel !</code>"
@@ -14,8 +14,9 @@ class Presets(object):
     BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
                   "available in our DB !</code>"
     PM_ERROR = "<b>Unable to send medias</b> ⛔️\n<code>Click the below button\nAsk here for movies later!</code>"
-    MEDIA_SEND_TEXT = "<code>Media dispatched as PM 🥳</code>"
-    NO_MEDIA = "Requested movie: <b>{}</b>\n\n<b>Not available " \
+    MEDIA_SEND_TEXT = "<code>Media dispatched as PM 🥳
+@request_moviesorseries </code>"
+    NO_MEDIA = "Requested movie: <b>{}</b>\n\n<b>Not available in @request_moviesorseries " \
                "Right Now</b>\n<code>Possible Causes : 🤔\n\n⭕️ Not " \
                "released yet</code>\n⭕️ <a href='https://www.google.com/search?q={}'> Spelled incorrectly</a>\n" \
                "<code>⭕️ Unwanted texts in Msgs\n⭕ Asking theatre prints\n⭕ Not in ma Database</code>"
