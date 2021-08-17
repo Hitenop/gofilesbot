b2c9219f080581733847653af0169c741b5804bb
Hitenop <87300913+Hitenop@users.noreply.github.com>
2021-08-17 17:24:17 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -17,5 +17,5 @@ class Presets(object):
     NO_MEDIA = "Requested movie: <b>{}</b>\n\n<b>Not available in @request_moviesorseries " \
                "Right Now</b>\n<code>Possible Causes : 🤔\n\n⭕️ Not " \
                "released yet</code>\n⭕️ <a href='https://www.google.com/search?q={}'> Spelled incorrectly</a>\n" \
-               "<code>⭕️ Unwanted texts in Msgs\n⭕ Asking theatre prints\n⭕ Not in ma Database</code>" ⭕ask admins to upload it </code>"
+               "<code>⭕️ Unwanted texts in Msgs\n⭕ Asking theatre prints\n⭕ Not in ma Database</code>" 
     BLOCK_LIST = ['http://', 'https://', '@', '#', 'bit.ly', 't.me', '/']
