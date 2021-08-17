80fbc9a99f6a91af0e978abcbf00aa62609f7b49
Hitenop <87300913+Hitenop@users.noreply.github.com>
2021-08-17 17:26:23 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -13,7 +13,7 @@ class Presets(object):
     BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
                   "available in our DB !</code>"
     PM_ERROR = "<b>Unable to send medias</b> ⛔️\n<code>Click the below button\nAsk here for movies later!</code>"
-    MEDIA_SEND_TEXT = "<code>Media dispatched as PM 🥳 </code>"
+    MEDIA_SEND_TEXT = "Media dispatched as PM 🥳 @request_moviesorseries</code>"
     NO_MEDIA = "Requested movie: <b>{}</b>\n\n<b>Not available in @request_moviesorseries " \
                "Right Now</b>\n<code>Possible Causes : 🤔\n\n⭕️ Not " \
                "released yet</code>\n⭕️ <a href='https://www.google.com/search?q={}'> Spelled incorrectly</a>\n" \
